c1b7b215f315a6c97c514c89f20d4210a3b735b8
fadhilrachman <muhfadhilrachman@gmail.com>
2025-12-16 18:50:54 +0700

    fix: added overstock status

--- components/features/stock-on-hand/TableOnHand.vue
+++ components/features/stock-on-hand/TableOnHand.vue
@@ -104,6 +104,7 @@ const statusOptions = [
   { id: "available", label: "Available" },
   { id: "low", label: "Low" },
   { id: "out_of_stock", label: "Out of Stock" },
+  { id: "overstock", label: "Overstock" },
 ];
 
 const handleFacilitiesChange = (value: any) => {
